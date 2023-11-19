07577fa280cfe7bdb8c939f8d6869d99440339c7
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2023-11-19 22:59:28 +0200

    fixed js-hw-01

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
+// Обчислює загальну ширину елемента.
 function getElementWidth(content, padding, border) {
-    const commonWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
- return commonWidth;
+ const totalWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+ return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
